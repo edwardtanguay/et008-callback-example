85d05b1baef9ec6e92373815f1de9a4192766845
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-11 19:27:46 +0200

    up to mock wait

--- src/components/EmployeeDataLoader.ts
+++ src/components/EmployeeDataLoader.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
+import { IEmployee } from '../interfaces';
 
 const url = 'https://edwardtanguay.vercel.app/share/employees.json';
 
-export const EmployeeDataLoader = () => {
+export const EmployeeDataLoader = (callback: (employees: IEmployee[]) => void) => {
 	(async () => {
-		const employees = (await axios.get(url)).data;
-		console.log(employees);
-		return employees;
+		const employees:IEmployee[] = (await axios.get(url)).data;
+		callback(employees);
 	})();
-	return 'nothing';
 };
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,20 @@ import { EmployeeDataLoader } from './components/EmployeeDataLoader';
 import './style.scss';
 
 const colors = ColorDataLoader();
-const employees = EmployeeDataLoader();
-console.log(employees);
+
+EmployeeDataLoader((employees) => {
+	document.querySelector<HTMLDivElement>('.employeeInfo')!.innerHTML = `
+	There are ${employees.length} employees:
+	<ul>
+	${employees.map(employee => {
+		return `<li>${employee.firstName} ${employee.lastName}</li>`;
+	}).join('')}	
+	</ul>
+	`;
+});
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <h1>Callback Demo Site</h1>
 <p>There are ${colors.length} colors: ${colors.map(m => `${m}`).join(', ')}</p>
+<p class="employeeInfo"></p>
 `;
